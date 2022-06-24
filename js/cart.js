e9391f2f622bb7421cc56493640ab7e452747034
Lucy Gelderloos <lucygel@gmail.com>
2022-06-23 17:40:54 -0700

    All working but RemoveItem

--- js/cart.js
+++ js/cart.js
@@ -3,7 +3,8 @@
 
 // Create an event listener so that when the delete link is clicked, the removeItemFromCart method is invoked.
 const table = document.getElementById('cart');
-table.addEventListener('click', removeItemFromCart);
+// table.addEventListener('click', removeItemFromCart);
+
 let cart;
 
 function loadCart() {
@@ -19,25 +20,55 @@ function renderCart() {
 }
 
 // TODO: Remove all of the rows (tr) in the cart table (tbody)
-function clearCart() {}
+function clearCart() {
+  let cartTable = document.querySelector('#cart > tbody')
+  while (cartTable.firstChild) {
+    cartTable.removeChild(cartTable.firstChild);
+}
+}
 
 // TODO: Fill in the <tr>'s under the <tbody> for each item in the cart
 function showCart() {
 
   // TODO: Find the table body
-
+  let cartTable = document.querySelector('#cart > tbody')
+  
   // TODO: Iterate over the items in the cart
+  for(let i = 0; i < cart.items.length; i++){
+  
   // TODO: Create a TR
+  let cartRow = document.createElement('tr');
+  
   // TODO: Create a TD for the delete link, quantity,  and the item
-  // TODO: Add the TR to the TBODY and each of the TD's to the TR
+  let deleteCell = document.createElement('td');
+  let deleteLink = document.createElement('a');
+  deleteLink.id = cart.items[i].product;
+  deleteLink.appendChild(document.createTextNode('X'));
+  deleteLink.addEventListener('click', removeItemFromCart);
 
+  let qtyCell = document.createElement('td');
+  qtyCell.appendChild(document.createTextNode(cart.items[i].quantity));
+
+  let itemCell = document.createElement('td');
+  itemCell.appendChild(document.createTextNode(cart.items[i].product));
+  
+  // TODO: Add the TR to the TBODY and each of the TD's to the TR
+  cartTable.appendChild(cartRow);
+  cartRow.appendChild(deleteCell);
+  cartRow.appendChild(qtyCell);
+  cartRow.appendChild(itemCell);
+
+  }
 }
 
 function removeItemFromCart(event) {
 
   // TODO: When a delete link is clicked, use cart.removeItem to remove the correct item
+  cart.removeItem()
   // TODO: Save the cart back to local storage
+  cart.saveToLocalStorage();
   // TODO: Re-draw the cart table
+  showCart()
 
 }
 
